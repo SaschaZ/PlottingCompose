f601eff7e993a2506c8af380a016ddb5fe7f1692
Sascha Zieger <sascha@zieger.dev>
2021-11-23 22:47:23 +0100

    fix

--- applyTag.sh
+++ applyTag.sh
@@ -3,4 +3,4 @@
 echo "applying tag $1"
 
 sed -E 's/projectVersion="[0-9]+\.[0-9]+\.[0-9]+"/projectVersion="'${1:1}'"/g' gradle.properties > gradle.properties2
-mv gradle.properties gradle.properties
+mv gradle.properties2 gradle.properties
