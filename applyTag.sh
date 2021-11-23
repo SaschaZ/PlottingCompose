ae26365b04ba447fe94ff075993088a1a5f502d5
Sascha Zieger <sascha@zieger.dev>
2021-11-23 23:40:37 +0100

    credentials fix

--- applyTag.sh
+++ applyTag.sh
@@ -4,3 +4,9 @@ echo "applying tag $1"
 
 sed -E 's/projectVersion=[0-9]+\.[0-9]+\.[0-9]+/projectVersion='${1:1}'/g' gradle.properties > gradle.properties2
 mv gradle.properties2 gradle.properties
+
+sed -E 's/ziegerDevReleasesUsername=/ziegerDevReleasesUsername='$2'/g' gradle.properties > gradle.properties2
+mv gradle.properties2 gradle.properties
+
+sed -E 's/ziegerDevReleasesPassword=/ziegerDevReleasesPassword='$3'/g' gradle.properties > gradle.properties2
+mv gradle.properties2 gradle.properties
